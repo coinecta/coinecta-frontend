1d2b6eca6e83e434b2ade8485046077d540f135c
esot321c <esot321c@gmail.com>
2023-11-17 14:24:31 -0800

    udpate whitelist admin area

---
+++ src/components/admin/whitelist/SumsubId.tsx
@@ -0,0 +1,131 @@
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { trpc } from '@lib/utils/trpc';
+import { useAlert } from '@contexts/AlertContext';
+import Grid from '@mui/system/Unstable_Grid/Grid';
+import { resolveRewardAddress, resolveStakeKeyHash } from '@meshsdk/core';
+
+const SumsubId: FC = () => {
+  const { addAlert } = useAlert();
+  const [adaAddress, setAdaAddress] = useState<string>('')
+
+  const initCustomAddress = {
+    rewardAddress: '',
+    changeAddress: ''
+  }
+  const [customRewardAddress, setCustomRewardAddress] = useState(initCustomAddress)
+  const userInfo = trpc.whitelist.getSumsubId.useQuery(customRewardAddress, {
+    enabled: customRewardAddress.changeAddress.length > 0 || customRewardAddress.rewardAddress.length > 0,
+    retry: false
+  })
+  const [refetchUserInfo, setRefetchUserInfo] = useState(false)
+  useEffect(() => {
+    if (refetchUserInfo) {
+      userInfo.refetch()
+      setRefetchUserInfo(false)
+    }
+  }, [refetchUserInfo])
+  const handleChangeAdaAddress = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setAdaAddress(e.target.value)
+  }
+  const deriveCustomRewardAddress = (address: string) => {
+    if (address === '') {
+      setCustomRewardAddress(initCustomAddress)
+      return
+    }
+    try {
+      // this tells us that address is a change address and potentially the user's defaultAddress
+      // it also derives the stake address. we can check the database for both to find a user account. 
+      const rewardAddress = resolveRewardAddress(address);
+      console.log(rewardAddress)
+      if (rewardAddress) {
+        setCustomRewardAddress({
+          rewardAddress,
+          changeAddress: address
+        });
+      }
+    } catch (error) {
+      try {
+        // this tells us its a valid stake key
+        const rewardAddress = resolveStakeKeyHash(address);
+        console.log(rewardAddress)
+        if (rewardAddress) {
+          setCustomRewardAddress({
+            rewardAddress: address,
+            changeAddress: ''
+          });
+        }
+      } catch (error) {
+        console.log(error)
+        setCustomRewardAddress(initCustomAddress);
+        addAlert('error', 'Please enter a valid Cardano address');
+      }
+    }
+  };
+
+  return (
+    <Box>
+      <Typography variant="h6" sx={{ mb: 1 }}>
+        Get SumSub ID
+      </Typography>
+      <Grid container sx={{ mb: 2 }} spacing={2} alignItems="center">
+        <Grid xs>
+          <TextField
+            id="wallet-address"
+            variant="filled"
+            label="Address or stake key"
+            value={adaAddress}
+            onChange={handleChangeAdaAddress}
+            fullWidth
+          />
+        </Grid>
+        <Grid xs="auto">
+          <Button
+            variant="contained"
+            onClick={() => deriveCustomRewardAddress(adaAddress)}>
+            Submit
+          </Button>
+        </Grid>
+        <Grid xs="auto">
+          <Button variant="outlined"
+            onClick={() => {
+              setAdaAddress('')
+              deriveCustomRewardAddress('')
+            }}
+          >
+            Clear
+          </Button>
+        </Grid>
+      </Grid>
+      <Box>
+        {userInfo.isError && userInfo.error.shape?.message.includes('Unable to retrieve user')
+          ? <Box>
+            User not found in db
+          </Box>
+          : userInfo?.data?.sumsubId
+            ? <Box>
+              <Typography>
+                Sumsub ID: {userInfo?.data?.sumsubId}
+              </Typography>
+              <Typography>
+                Sumsub Result: {JSON.stringify(userInfo?.data?.sumsubResult)}
+              </Typography>
+              <Typography>
+                Sumsub Status: {userInfo?.data?.sumsubStatus}
+              </Typography>
+            </Box>
+            : <Box>Please enter a valid address</Box>
+        }
+      </Box>
+    </Box>
+  );
+};
+
+export default SumsubId;
+
+
--- src/server/routers/whitelist.ts
+++ src/server/routers/whitelist.ts
@@ -1,8 +1,29 @@
 import { prisma } from '@server/prisma';
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { adminProcedure, createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const whitelistRouter = createTRPCRouter({
+  getNumUsers: adminProcedure
+    .query(async () => {
+      const totalUsers = await prisma.user.count();
+      return totalUsers
+    }),
+  getNumSignups: adminProcedure
+    .query(async () => {
+      const counts = await prisma.whitelistSignup.groupBy({
+        by: ['whitelist_slug'],
+        _count: {
+          whitelist_slug: true,
+        },
+      });
+      return counts;
+    }),
+  listWhitelists: adminProcedure
+    .query(async () => {
+      const whitelists = await prisma.whitelist.findMany();
+      return whitelists
+    }),
   submitWhitelist: protectedProcedure
     .input(z.object({
       whitelistSlug: z.string(),
@@ -126,6 +147,47 @@ export const whitelistRouter = createTRPCRouter({
           code: 'INTERNAL_SERVER_ERROR',
           error: error
         }
+      }
+    }),
+  getSumsubId: adminProcedure
+    .input(z.object({
+      changeAddress: z.string().optional(),
+      rewardAddress: z.string().optional()
+    }))
+    .query(async ({ input }) => {
+      const { changeAddress, rewardAddress } = input
+
+      const orConditions = [];
+
+      // Add conditions to the array only if the addresses are provided
+      if (changeAddress) {
+        orConditions.push({ defaultAddress: changeAddress });
+      }
+      if (rewardAddress) {
+        orConditions.push({ rewardAddress: rewardAddress });
+      }
+
+      try {
+        const sumsub = await prisma.user.findFirst({
+          where: {
+            OR: orConditions
+          }
+        });
+
+        if (sumsub) {
+          return sumsub
+        }
+        else {
+          throw new TRPCError({
+            message: 'Unable to retrieve user',
+            code: 'NOT_FOUND'
+          })
+        }
+      } catch (error) {
+        throw new TRPCError({
+          message: `Error: ${error}`,
+          code: "INTERNAL_SERVER_ERROR"
+        })
       }
     }),
 })
