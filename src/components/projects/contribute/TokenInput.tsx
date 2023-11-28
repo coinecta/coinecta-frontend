af0107236549c10a69ac56ad43c000af423cdbca
esot321c <esot321c@gmail.com>
2023-11-28 09:49:12 -0800

    quick fix inputs

--- src/components/projects/contribute/TokenInput.tsx
+++ src/components/projects/contribute/TokenInput.tsx
@@ -61,7 +61,7 @@ const TokenInput: FC<ITokenInputProps> = ({
       // Convert to a number for output value, handling potential NaN
       const numericValue = Number(rawValue);
       if (!isNaN(numericValue)) {
-        setOutputValue(numericValue.toLocaleString(undefined, { maximumFractionDigits: 2 }));
+        setOutputValue(numericValue.toFixed(0));
       } else {
         setOutputValue('');
       }
