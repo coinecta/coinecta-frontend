2d7b0eae2635f150fa0e99f4f7de1c4675fa339f
esot321c <esot321c@gmail.com>
2024-09-16 21:01:28 -0700

    navigate to active round

--- src/components/projects/contribute/SaleTerm.tsx
+++ src/components/projects/contribute/SaleTerm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
+import MarkdownRender from '@components/MarkdownRender';
 
 interface SalesTermProps {
   header: string;
@@ -12,9 +13,11 @@ const SalesTerm: React.FC<SalesTermProps> = ({ header, bodyText }) => {
       <Typography variant="h6" sx={{ mb: 1, fontSize: '1.2rem!important' }}>
         {header}
       </Typography>
-      <Typography variant="body1" sx={{ fontSize: '1rem!important' }}>
-        {bodyText}
-      </Typography>
+      {/* <Typography variant="body1" sx={{ fontSize: '1rem!important' }}> */}
+      <Box sx={{ '& p': { fontSize: '1rem!important' } }}>
+        <MarkdownRender description={bodyText} />
+      </Box>
+      {/* </Typography> */}
     </Box>
   );
 };
